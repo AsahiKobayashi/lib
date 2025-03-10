40b2ae2b3e91f87382fe2bb641ae363bf6d15b51
小林　朝陽 <109237514+AsahiKobayashi@users.noreply.github.com>
2025-03-10 11:41:24 +0900

    Update Generator.java

--- generate/Generator.java
+++ generate/Generator.java
@@ -1,9 +1,15 @@
 final class Generator {
     
-    public static <T> List<T> [] listArray(int n , Class<T> clazz) {
+    public static final <T> List<T> [] generateListArray(int n , Class<T> clazz) {
         List<T> [] G = new ArrayList[n];
         Arrays.setAll(G , i -> new ArrayList<>());
         return G;
+    }
+    
+    public static final int [] serialNumber(int n) {
+        int [] array = new int[n];
+        Arrays.setAll(array , i -> i);
+        return array;
     }
     
 }
